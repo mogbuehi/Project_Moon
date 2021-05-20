a1894efa577ebd8e65f7cd752077624660248b5d
mogbuehi <38775809+mogbuehi@users.noreply.github.com>
2021-05-20 07:33:46 -0700

    commit before presentation, MVP

---
+++ Data/SPY_FAANGT_data.py
@@ -0,0 +1,51 @@
+# Importing Timeseries function from alpha_vantage SDK
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import os
+from dotenv import load_dotenv
+import time
+from sys import exit
+
+# Make function for collecting data from Alpha Vantage, so that module doesn't run when imported
+def SPY_FAANGT_data():
+    '''
+    Parameters:
+    None. Function collects 20+ years of FAANGT and SPY (SP500 ETF) price data
+
+    *****************************************************************************************************************************************************
+    Returns: 
+    Returns .csv files into "Data/csv_files/" directory (overwrites/updates existing .csv of same name) and 2 dataframes
+    1.) FAANGT_df : a dataframe of FAANGT stocks (20+ years of data) from AlphaVantage API calls
+    2.) FAANGT_close_df : a dataframe of only the closing prices from FAANGT_df 
+    3.) FAANGT_close_data.csv : .csv file of FAANGT_close_df
+    4.) FAANGT_data.csv : .csv file of FAANGT_df
+    '''
+
+    # Load in API key
+    load_dotenv()
+    alpha_vantage_key = os.getenv('ALPHAVANTAGE_API_KEY')
+    # Confirm the availability of your Mapbox API access token by checking its type
+    if type(alpha_vantage_key) == type(''):
+        print('Key is available. SPY and FAANGT data is now loading (~5 mins)...')
+    else: 
+        exit('Try loading key again')
+
+    ts = TimeSeries(key = alpha_vantage_key, output_format='pandas') #Note 500 requests per day cap, 5 per min
+    SPY_FAANGT_list=['SPY','FB','AAPL','AMZN','NFLX', 'GOOG', 'TSLA']
+    SPY_FAANGT_df_list = [] #iniate df list for later concatenation
+    SPY_FAANGT_close_df_list = [] #iniate df list for just close prices later concatenation
+    for ticker in SPY_FAANGT_list:
+        ticker_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full') #using Alpha Vantage API to grab S&P 500 ETF (ticker symbol = SPY), gives 20+ years of price data
+        ticker_data.columns = [ticker + ' open', ticker + ' high', ticker + ' low', ticker + ' close', ticker + ' volume'] #rename columns automatically
+        SPY_FAANGT_df_list.append(ticker_data) #add dataframe to the list
+        SPY_FAANGT_close_df_list.append(ticker_data[ticker + ' close']) # Just the 'close' column
+        time.sleep(25) #Note 500 requests per day cap, 5 per min
+
+    SPY_FAANGT_df = pd.concat(SPY_FAANGT_df_list, axis=1, join='outer')
+    SPY_FAANGT_close_df = pd.concat(SPY_FAANGT_close_df_list, axis=1, join='outer')
+
+    # These directories will work only when called as function in Project Moon Notebook (DO NOT RUN in this notebook if in DATA/ directory or subdirs)
+    SPY_FAANGT_close_df.to_csv('Data/csv_files/SPY_FAANGT_close_data.csv') 
+    SPY_FAANGT_df.to_csv('Data/csv_files/SPY_FAANGT_data.csv')     
+
+    return SPY_FAANGT_df, SPY_FAANGT_close_df
--- scripts/perf_indicators.py
+++ scripts/perf_indicators.py
@@ -6,47 +6,31 @@ from pathlib import Path
 import numpy as np
 
 
-def get_crypto_coins(): # Make function for collecting data from Coin Gecko, so that module doesn't run when imported
-
-# Using the read_csv function and the Path module, read in the "crypto_price_data.csv" file from the Resources folder
-# Set the "date" as the index column 
-# Be sure to set the DatetimeIndex using the parse_dates and infer_datetime_format parameters
-    crypto_coins_df = pd.read_csv(
-        Path("/Users/craymundo/Documents/GitHub/Project_Moon/Data/crypto_price_data.csv"), 
-        index_col="time", 
-        infer_datetime_format=True, 
-        parse_dates=True
-    )
-    return crypto_coins_df
-
-
-
 #********************************************************************#
 # ** Perfomance indicators **  - Percentage change.
-def get_pct_change(crypto_coins_df): # Make function for collecting data from Coin Gecko, so that module doesn't run when imported
+def get_pct_change(dataframe): # Make function for collecting data from Coin Gecko, so that module doesn't run when imported
 # Using the Pandas pct_change function in conjunction with the dropna function, generate the tech daily returns DataFrame
 #alt_bitcoin_daily_returns = crypto_coins_df.pct_change().dropna()
-    crypto_daily_returns_df = crypto_coins_df.pct_change()
-    return crypto_daily_returns_df
+    pct_change_df = dataframe.pct_change(fill_method='bfill') #https://www.geeksforgeeks.org/python-pandas-series-pct_change/  may solve issue with Nan
+    return pct_change_df
 # ** Perfomance indicators **  - Cumulative percent returns.
-def get_cumulative_prod(crypto_daily_returns_df):
-# Calculating and ploting the cumulative returns including BTC
+def get_cumulative_returns(dataframe):
+# Calculating and ploting the cumulative returns
 # Using the Pandas cumprod function and the cumulative returns equation to generate the cumulative daily returns DataFrame 
-    get_cumulative_prod_df = (1 + crypto_daily_returns_df).cumprod()
+    get_cumulative_returns_df = (1 + get_pct_change(dataframe)).cumprod()
 # Reviewing the last 5 rows of whales cumulative daily returns DataFrame
-    return get_cumulative_prod_df
+    return get_cumulative_returns_df
 # ** Perfomance indicators **  - Rolling mean.
 # ** Perfomance indicators **  - Rolling mean returns.
 #********************************************************************#
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 # ++ Volatility ++ - Standard deviation.
-def get_standard_deviation(crypto_daily_returns_df):
+def get_standard_deviation(dataframe):
 # Calculating and sort the standard deviation for all ALT coins
 # Displaying the standard deviations sorted smallest to largest
-    standard_deviation_df = crypto_daily_returns_df.std().sort_values()  
+    standard_deviation_df = dataframe.fillna(0).std().sort_values()
 # Sorting the annual standard deviations for the cryptocurrencies from lowest to highest
-#standard_deviation.sort_values()
     return standard_deviation_df
 # ++ Volatility ++ - Annualized Standard deviation.
 
@@ -55,35 +39,32 @@ def get_standard_deviation(crypto_daily_returns_df):
 
 #********************************************************************#
 # ** Risk-Return **  - Beta(60 day rolling covariance).
-def get_standard_deviation(crypto_daily_returns_df):
+def get_rolling_beta(asset_df, market_df, days=60):
 
-# Calculating the variance using a 60-day rolling window
-    #snp_rolling_60_variance = crypto_daily_returns_df['S&P 500'].rolling(window=60).var()
-    beta_rolling_60_variance = crypto_daily_returns_df.rolling(window=60).var()
-# Calculating the covariance using a 60-day rolling window
-    #beta__rolling_60_covariance = crypto_daily_returns_df['BERKSHIRE HATHAWAY INC'].rolling(window=60).cov(crypto_daily_returns_df['S&P 500'])
-    beta__rolling_60_covariance = crypto_daily_returns_df.rolling(window=60).cov(crypto_daily_returns_df)
-# Calculating the beta based on the 60-day rolling covariance compared to the S&P 500
-    rolling_60_beta_df = beta__rolling_60_covariance / beta_rolling_60_variance
-    return rolling_60_beta_df
+# Calculating the variance using a  rolling window (default is 60-day)
+    #beta = cov(asset_df, market_df)/var(market_df)
+# Calculating the rolling covariance 
+    rolling_variance = market_df.rolling(window=days).var()
+    rolling_covariance = asset_df.pct_change().rolling(window=days).cov(market_df)
+# Calculating the rolling beta 
+    rolling_beta_df = rolling_covariance / rolling_variance
+    return rolling_beta_df
 
 # ** Risk-Return **  - Sharpe.
-def get_sharpe_rations(standard_deviation_df,crypto_daily_returns_df):
-# The number of trading days is set to 252 for use throughout these calculations
-    trading_days = 252
+def get_sharpe_ratios(dataframe, trading_days = 252):
+# The number of trading days is set to 252 for use throughout these calculations, though crypto generally trade 365 days a year
 # Calculate the annualized standard deviation for the fund portfolios and the S&P 500
 # Use the Pandas std function to calculate the standard deviation
 # Multiply the standard deviation by the square root (using the NumPy sqrt function) of the number of trading days
-    annual_average_return = crypto_daily_returns_df.mean() * np.sqrt(trading_days)
+    annualized_average_return = dataframe.mean() * (trading_days)
 
-# Calculating and sorting the annualized standard deviation (252 trading days) of the 4 portfolios and the S&P 500
-# Displaying the annual standard deviations smallest to largest
-    annual_standard_deviation = standard_deviation_df * np.sqrt(trading_days)
-   
+# Calculating and sorting the annualized standard deviation (252 trading days)
+    annualized_standard_deviation = get_standard_deviation(dataframe) * np.sqrt(trading_days)
 
 # Calculate Sharpe ratios by dividing the annual average return of the fund portfolios and the S&P 500
 # by the annual standard deviation
-    sharpe_ratios_df = annual_average_return / annual_standard_deviation
+    sharpe_ratios_df = annualized_average_return / annualized_standard_deviation
+    
     return sharpe_ratios_df
 
 #********************************************************************#
